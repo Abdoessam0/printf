f6c708c1f33913e7d008c6038ede621bd1055401
Abdoessam0 <aabbddo321@gmail.com>
2023-07-25 14:42:41 -0700

    updated

--- main.h
+++ main.h
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 
 /**
- * struct convention_c - convention characters indicator
+ * struct convention_char - convention characters indicator
  * @c: The convention char
  * @fun: The function of the char
  */
@@ -17,12 +17,11 @@ int _printf(const char *format, ...);
 int format_fun(const char *format, va_list args, conven_t con[]);
 int int_len(int i);
 int print_decimal(int n);
-int print_binary(unsigned int n);
+int print_binary(va_list arg);
 int write_fun(char c);
 int print_char(va_list arg);
 int print_str(va_list arg);
 int print_int(va_list arg);
 int print_percent(va_list arg);
-int print_bin(va_list arg);
 
 #endif
